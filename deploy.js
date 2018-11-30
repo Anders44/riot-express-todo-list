127f3f542d175d659d56815f8e2fb607b60b10a9
Anders <anderskornkven@gmail.com>
2018-11-29 18:08:48 -0700

    fixed something I hope

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.238.165.110',
+      host: '18.209.158.11',
       username: 'ubuntu',
       privateKey: 'tl-key.pem'
     })
