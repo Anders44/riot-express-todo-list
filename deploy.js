2a5af5c0372ff34e87952f9a163a84996d5e30ec
Anders <anderskornkven@gmail.com>
2018-11-29 22:43:26 -0700

    trying other key

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '35.175.228.154',
       username: 'ubuntu',
-      privateKey: 'tl-key.pem'
+      privateKey: 'hs-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
